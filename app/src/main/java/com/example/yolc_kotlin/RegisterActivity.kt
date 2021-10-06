1787ae357338af4b6e6c89ebd57d5f4c1c96dced
Hin1209 <hyeong5745@gmail.com>
2021-10-07 00:02:51 +0900

    Add function to certify by messaging

--- app/src/main/java/com/example/yolc_kotlin/RegisterActivity.kt
+++ app/src/main/java/com/example/yolc_kotlin/RegisterActivity.kt
@@ -1,13 +1,15 @@
 package com.example.yolc_kotlin
 
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.yolc_kotlin.InF.AuthService
+import com.example.yolc_kotlin.InF.RegisterService
+import com.example.yolc_kotlin.data.Auth
+import com.example.yolc_kotlin.data.GetAuth
 import com.example.yolc_kotlin.data.Login
 import kotlinx.android.synthetic.main.activity_registration.*
 import retrofit2.Call
@@ -15,36 +17,70 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
 
 class RegisterActivity : AppCompatActivity() {
 
     val TAG: String = "Register"
     private val BASE_URL = "http://ec2-18-191-209-53.us-east-2.compute.amazonaws.com"
 
-    interface RegisterService{
-        @FormUrlEncoded
-        @POST("signup/")
-        fun requestSignUp(
-            @Field("username") username: String,
-            @Field("password") password: String
-        ) : Call<Login>
-    }
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        val intent = Intent(this, LoginActivity::class.java)
-
-        var retrofit = Retrofit.Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        val registerService: RegisterService = retrofit.create(RegisterService::class.java)
+        val authService: AuthService = retrofit.create(AuthService::class.java)
+        var checkAuth = false
 
-        var registerService: RegisterService = retrofit.create(RegisterService::class.java)
+        sendSms.setOnClickListener{
+            Log.d(TAG,"인증번호 전송 버튼 클릭")
+            val phone_number = textPhone.text.toString()
+            if(phone_number.length == 13) {
+                authService.smsAuth(phone_number).enqueue(object : Callback<Auth> {
+                    override fun onFailure(call: Call<Auth>, t: Throwable) {
+                        Log.e("AUTH", "${t.message}")
+                        var dialog = AlertDialog.Builder(this@RegisterActivity)
+                        dialog.setTitle("알림")
+                        dialog.setMessage("통신에 실패했습니다.")
+                        dialog.show()
+                    }
+
+                    override fun onResponse(call: Call<Auth>, response: Response<Auth>) {
+                        var auth = response.body()
+                        Log.d(TAG, auth.toString())
+                    }
+                })
+            }
+        }
+
+        phoneAuth.setOnClickListener{
+            val auth_number = smsAuth.text.toString()
+            val phone_number = textPhone.text.toString()
+            if(phone_number.length == 13 && auth_number.length == 4) {
+                authService.getAuth(phoneNum = phone_number, authNum = auth_number)
+                    .enqueue(object : Callback<GetAuth> {
+                        override fun onFailure(call: Call<GetAuth>, t: Throwable) {
+                            Log.e("GetAuth", "${t.message}")
+                            var dialog = AlertDialog.Builder(this@RegisterActivity)
+                            dialog.setTitle("알림")
+                            dialog.setMessage("통신에 실패했습니다")
+                            dialog.show()
+                        }
+
+                        override fun onResponse(call: Call<GetAuth>, response: Response<GetAuth>) {
+                            val res = response.body()
+                            if (res?.result == true) {
+                                checkAuth = true
+                                Log.d(TAG, res.result.toString())
+                                dialogShow("auth success")
+                            }
+                        }
+                    })
+            }
+        }
 
         btn_register2.setOnClickListener{
             Log.d(TAG,"회원가입 버튼 클릭")
@@ -57,10 +93,6 @@ class RegisterActivity : AppCompatActivity() {
             val pw = edit_pw.text.toString()
             val pw_re = edit_pw_re.text.toString()
 
-            Log.d(TAG,id)
-            Log.d(TAG,pw)
-            Log.d(TAG,pw_re)
-
             if(id.isEmpty() || pw.isEmpty() || pw_re.isEmpty()){
                 isExistBlank = true
             } else{
@@ -72,7 +104,6 @@ class RegisterActivity : AppCompatActivity() {
             if(!isExistBlank && isPWSame){
                 registerService.requestSignUp(id, pw).enqueue(object: Callback<Login>{
                     override fun onFailure(call: Call<Login>, t:Throwable){
-
                         Log.e("SIGNUP", "${t.message}")
                         var dialog = AlertDialog.Builder(this@RegisterActivity)
                         dialog.setTitle("알림")
@@ -81,30 +112,23 @@ class RegisterActivity : AppCompatActivity() {
                     }
                     override fun onResponse(call: Call<Login>, response: Response<Login>){
                         var login = response.body()
-
                         if(login?.code == "1002"){
                             Log.d(TAG,"이미 존재하는 ID입니다.")
                             isExistID = true
                         }
                     }
                 })
-
                 if(!isExistID) {
-                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
-
-                    val sharedPreference = getSharedPreferences("file name", Context.MODE_PRIVATE)
-
-                    val editor = sharedPreference.edit()
-                    editor.putString("id", id)
-                    editor.putString("pw", pw)
-                    editor.apply()
-
-                    val intent = Intent(this, LoginActivity::class.java)
-                    startActivity(intent)
+                    if(checkAuth) {
+                        dialogShow("success")
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else{
+                        dialogShow("not auth")
+                    }
                 } else dialogShow("Exist ID")
 
             } else{
-
                 if(isExistBlank){
                     dialogShow("blank")
                 } else if(!isPWSame){
@@ -116,7 +140,6 @@ class RegisterActivity : AppCompatActivity() {
 
     fun dialogShow(type: String){
         val dialog = AlertDialog.Builder(this)
-
         if(type.equals("blank")){
             dialog.setTitle("회원가입 실패")
             dialog.setMessage("입력란을 모두 작성해주세요")
@@ -126,17 +149,24 @@ class RegisterActivity : AppCompatActivity() {
         } else if(type.equals("Exist ID")){
             dialog.setTitle("회원가입 실패")
             dialog.setMessage("이미 존재하는 ID 입니다.")
+        } else if(type.equals("not auth")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("휴대폰 인증을 완료해주세요")
+        } else if(type.equals("success")){
+            dialog.setTitle("회원가입 성공!")
+            dialog.setMessage("회원가입에 성공하였습니다!")
+        } else if(type.equals("auth success")){
+            dialog.setTitle("인증 성공")
+            dialog.setMessage("인증이 완료되었습니다")
         }
-
-        val dialog_listener = object: DialogInterface.OnClickListener{
-            override fun onClick(dialog: DialogInterface?, which: Int){
-                when(which){
+        val dialog_listener = object: DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
                     DialogInterface.BUTTON_POSITIVE ->
-                        Log.d(TAG,"다이얼로그")
+                        Log.d(TAG, "다이얼로그")
                 }
             }
         }
-
         dialog.setPositiveButton("확인",dialog_listener)
         dialog.show()
     }
