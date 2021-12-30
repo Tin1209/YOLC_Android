76bdc66a60d5f2021fade5352ef16d450e1a64c5
Hin1209 <hyeong5745@gmail.com>
2021-12-31 00:15:55 +0900

    Add function to transaction fragments

--- app/src/main/java/com/example/yolc_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/yolc_kotlin/MainActivity.kt
@@ -3,22 +3,44 @@ package com.example.yolc_kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.MenuItem
+import androidx.fragment.app.FragmentTransaction
+import com.example.yolc_kotlin.Fragment.FragHome
+import com.example.yolc_kotlin.Fragment.FragProfile
+import com.example.yolc_kotlin.Fragment.FragRecord
+import com.example.yolc_kotlin.databinding.ActivityMainBinding
 
 public class MainActivity : AppCompatActivity(){
 
+    lateinit var binding: ActivityMainBinding
+    var flag = "home"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        allowEntry.isEnabled = false
-        test.setOnClickListener {
-            allowEntry.isEnabled = true
-        }
+        fun onNavigationItemSelected(p0: MenuItem): Boolean{
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
 
-        checkLog.setOnClickListener {
-            val intent = Intent(this, TempActivity::class.java)
-            startActivity(intent)
+            when(p0.itemId){
+                R.id.home -> {
+                    val fragment_home = FragHome()
+                    transaction.replace(R.id.main_layout, fragment_home, "home")
+                }
+                R.id.record ->{
+                    val fragment_record = FragRecord()
+                    transaction.replace(R.id.main_layout, fragment_record, "record")
+                }
+                R.id.profile ->{
+                    val fragment_profile = FragProfile()
+                    transaction.replace(R.id.main_layout, fragment_profile, "profile")
+                }
+            }
+            transaction.addToBackStack(null)
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            transaction.commit()
+            return true
         }
     }
 }
