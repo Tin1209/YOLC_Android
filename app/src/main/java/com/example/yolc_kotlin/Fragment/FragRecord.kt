b48644065aa1ee5686fef1dca4e60a28d8f0080d
Hin1209 <hyeong5745@gmail.com>
2021-12-31 00:14:10 +0900

    Add fragment class

---
+++ app/src/main/java/com/example/yolc_kotlin/Fragment/FragRecord.kt
@@ -0,0 +1,18 @@
+package com.example.yolc_kotlin.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.yolc_kotlin.R
+
+class FragRecord: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_record, container, false)
+    }
+}
