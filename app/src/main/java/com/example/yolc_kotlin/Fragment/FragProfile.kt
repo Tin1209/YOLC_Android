9b30072a533b858b558ae75bcf82abf69d292a03
Tin1209 <hyeongin2024@gmail.com>
2022-01-10 17:29:35 +0900

    Add Click Listener for Edit Profile Button

--- app/src/main/java/com/example/yolc_kotlin/Fragment/FragProfile.kt
+++ app/src/main/java/com/example/yolc_kotlin/Fragment/FragProfile.kt
@@ -1,17 +1,28 @@
 package com.example.yolc_kotlin.Fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.yolc_kotlin.R
+import com.example.yolc_kotlin.MainActivity
+import com.example.yolc_kotlin.databinding.FragmentProfileBinding
 
-class FragProfile: Fragment() {
+class FragProfile: Fragment(){
+    var mainActivity: MainActivity? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.editProfile.setOnClickListener{mainActivity?.goEditProfile()}
+
+        return binding.root
+    }
+    override fun onAttach(context: Context){
+        super.onAttach(context)
+        if(context is MainActivity) mainActivity = context
     }
 }
+
