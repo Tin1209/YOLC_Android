3c86457547624ae8eae6d6bfafdda4dc39adeabd
Tin1209 <hyeongin2024@gmail.com>
2022-01-29 22:39:21 +0900

    Add functions to save address and token

--- app/src/main/java/YolcSharedPreferences.kt
+++ app/src/main/java/YolcSharedPreferences.kt
@@ -4,6 +4,30 @@ import android.content.SharedPreferences
 object YolcSharedPreferences {
     private val ACCOUNT: String = "account"
 
+    fun setAddress(context: Context, input: String){
+        val prefs: SharedPreferences = context.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("address", input)
+        editor.commit()
+    }
+
+    fun getAddress(context: Context): String{
+        val prefs: SharedPreferences = context.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("address", "").toString()
+    }
+
+    fun setToken(context: Context, input: String){
+        val prefs: SharedPreferences = context.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("token", input)
+        editor.commit()
+    }
+
+    fun getToken(context: Context): String{
+        val prefs: SharedPreferences = context.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("token", "").toString()
+    }
+
     fun setUserId(context: Context, input: String){
         val prefs: SharedPreferences = context.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = prefs.edit()
