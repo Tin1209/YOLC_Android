6598768d5a54b0e52643e4ae0b141d2e10232a3a
Tin1209 <hyeongin2024@gmail.com>
2022-02-01 15:08:54 +0900

    Hide stream url

--- app/src/main/java/com/example/yolc_kotlin/OpenDoorActivity.kt
+++ app/src/main/java/com/example/yolc_kotlin/OpenDoorActivity.kt
@@ -7,13 +7,14 @@ import com.example.yolc_kotlin.databinding.ActivityOpenBinding
 
 class OpenDoorActivity: AppCompatActivity() {
 
+    private val app: url = url()
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         val bind = ActivityOpenBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
         val wView = bind.streaming
-
 
         val ws = wView.settings
         ws.setSupportMultipleWindows(false)
@@ -22,8 +23,6 @@ class OpenDoorActivity: AppCompatActivity() {
         ws.javaScriptEnabled
         ws.domStorageEnabled
         wView.webViewClient = WebViewClient()
-        wView.loadUrl("http://heean6620.iptime.org:12345")
-
-
+        wView.loadUrl(app.get_cam())
     }
 }
